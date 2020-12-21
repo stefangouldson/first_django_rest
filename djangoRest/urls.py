e3d9668b1d54bd86468e1c7921b0d400d02a2f50
Stefan Gouldson <sgouldson@CASPIAN71.caspian.local>
2020-12-21 21:44:44 +0000

    image field added

--- djangoRest/urls.py
+++ djangoRest/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from movies.views import MovieViewSet, ActionViewSet
+from django.conf.urls.static import static
+from django.conf import settings
 
 router = routers.SimpleRouter()
 router.register('movies', MovieViewSet)
@@ -25,4 +27,4 @@ router.register('action', ActionViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- movies/models.py
+++ movies/models.py
@@ -11,3 +11,4 @@ class MovieData(models.Model):
     duration = models.FloatField()
     rating = models.FloatField()
     typ = models.CharField(max_length=200, default='action')
+    image = models.ImageField(upload_to='Images/', default='Images/None/Noimg.jpg')
